b78f5fd867dd5915c7bafd2f2b4b57ab92358087
MacAnderson <macandersonuche@yahoo.com>
2021-11-23 23:22:45 +0000

    c1

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -2,7 +2,7 @@ let token;
 
 const clientID = "67d24e845d5e4399a8be009539002de8";
 // const redirectURI = "http://localhost:3000/";
-const redirectURI = "http://mac.surge.sh/";
+const redirectURI = "http://mac.surge.sh";
 
 
 const Spotify = {
